b8a403bafe121f015da19309805b21bb724575e0
Dimitar Chakarov <dimitar@dchakarov.com>
2021-12-11 16:44:25 +0000

    Both in one go today

--- Advent of Code/main.swift
+++ Advent of Code/main.swift
@@ -5,6 +5,7 @@
 
 import Foundation
 import RegexHelper
+import AppKit
 
 func main() {
     let fileUrl = URL(fileURLWithPath: "./aoc-input")
@@ -12,28 +13,66 @@ func main() {
     
     let lines = inputString.components(separatedBy: "\n")
         .filter { !$0.isEmpty }
-    
-    // Sample algorithm
-    var scoreboard = [String: Int]()
-    lines.forEach { line in
-        let (name, score) = parseLine(line)
-        scoreboard[name] = score
+
+    var energyLevels = lines.map { line in
+        return Array(line).map { Int(String($0))! }
     }
-    scoreboard
-        .sorted { lhs, rhs in
-            lhs.value > rhs.value
+    var totalFlashes = 0
+    let totalOctopuses = energyLevels[0].count * energyLevels.count
+    var allFlashed = false
+    var i = 0
+
+    while !allFlashed {
+        i += 1
+        let (levels, flashes) = runStep(initialEnergyLevels: energyLevels)
+        energyLevels = levels
+        if flashes == totalOctopuses {
+            allFlashed = true
+            print("Part 2: \(i)")
         }
-        .forEach { name, score in
-            print("\(name) \(score) pts")
+        totalFlashes += flashes
+        if i == 100 {
+            print("Part 1: \(totalFlashes)")
         }
+    }
 }
 
-func parseLine(_ line: String) -> (name: String, score: Int) {
-    let helper = RegexHelper(pattern: #"([\-\w]*)\s(\d+)"#)
-    let result = helper.parse(line)
-    let name = result[0]
-    let score = Int(result[1])!
-    return (name: name, score: score)
+func runStep(initialEnergyLevels: [[Int]]) -> ([[Int]], Int) {
+    var energyLevels = initialEnergyLevels
+    var pointsToAddress = [(x: Int, y: Int)]()
+    var pointsToNullify = [(x: Int, y: Int)]()
+    var flashes = 0
+
+    for i in 0 ..< energyLevels[0].count {
+        for j in 0 ..< energyLevels.count {
+            energyLevels[i][j] += 1
+            if energyLevels[i][j] > 9 {
+                pointsToAddress.append((x: i, y: j))
+                flashes += 1
+            }
+        }
+    }
+
+    while !pointsToAddress.isEmpty {
+        let point = pointsToAddress.removeFirst()
+        for i in max(point.x - 1, 0) ... min(point.x + 1, energyLevels[0].count - 1) {
+            for j in max(point.y - 1, 0) ... min(point.y + 1, energyLevels.count - 1) {
+                energyLevels[i][j] += 1
+                if energyLevels[i][j] == 10 {
+                    if !pointsToAddress.contains(where: { $0.x == i && $0.y == j }) {
+                        pointsToAddress.append((x: i, y: j))
+                        flashes += 1
+                    }
+                }
+            }
+        }
+        pointsToNullify.append(point)
+    }
+    pointsToNullify.forEach { point in
+        energyLevels[point.x][point.y] = 0
+    }
+
+    return (energyLevels, flashes)
 }
 
 main()
